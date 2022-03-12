a73f406061e51b229e1f719720e71ccb3c8b0611
igorugarteCVM <igorugarte.cvm@gmail.com>
2022-03-12 16:44:10 +0100

    prueba

--- week1/model.py
+++ week1/model.py
@@ -1,52 +1,55 @@
 import torch
-import torch.nn as nn
+from torch import nn
 
-class NetSquared(nn.Module):
-    """
-    Class of the custom CNN
-    """
+# Define model
+class NeuralNetwork(nn.Module):
     def __init__(self):
-        """
-        Initialization of the needed layers
-        """
-        super(NetSquared, self).__init__()
+        super(NeuralNetwork, self).__init__()
 
         self.relu = nn.ReLU()
-        self.maxp = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv1 = nn.Conv2d(3,32,kernel_size=(3,3))
+        self.conv2 = nn.Conv2d(32,64,kernel_size=(3,3))
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3,3))
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3))
+        self.maxpool = nn.MaxPool2d(kernel_size=(2,2))
 
-        self.bnorm1 = nn.BatchNorm2d(num_features=32)
-        self.bnorm2 = nn.BatchNorm2d(num_features=64)
+        self.batchnorm1 = nn.BatchNorm2d(num_features=32)
+        self.batchnorm2 = nn.BatchNorm2d(num_features=64)
 
-        self.dropout1 = nn.Dropout2d(p=0.1)
-        self.dropout2 = nn.Dropout2d(p=0.3)
+        self.dropout1 = nn.Dropout(p=0.1)
+        self.dropout2 = nn.Dropout(p=0.3)
 
-        self.linear = nn.Linear(in_features=246016, out_features=8)
+        self.linear1 = nn.Linear(64,64)
+        self.linear2 = nn.Linear(64,8)
+
+        self.globalAvgPooling = nn.AvgPool2d((62,62))
+
+        self.softmax = nn.Softmax()
+
 
     def forward(self, x):
+
         x = self.conv1(x)
         x = self.relu(x)
-        x = self.maxp(x)
-        x = self.bnorm1(x)
+        x = self.maxpool(x)
+        x = self.batchnorm1(x)
+        x = self.dropout1(x)
 
         x = self.conv2(x)
         x = self.relu(x)
-        x = self.maxp(x)
-        x = self.bnorm2(x)
+        x = self.maxpool(x)
+        x = self.batchnorm2(x)
+        x = self.dropout2(x)
 
+
+        x = self.globalAvgPooling(x)
         x = torch.squeeze(x)
         x = torch.flatten(x,1)
-        x = self.linear(x)
 
+        """ x = self.linear1(x) """
+        x = self.relu(x)
+        x = self.dropout1(x)
+
+        x = self.linear2(x)
+        x = self.softmax(x)
         return x
-
-
-if __name__ == "__main__":
-    """
-    Tests to see if there are errors
-    """
-    x = torch.randn((16, 3, 224, 224))      # 16 random images
-    model = NetSquared()                    # model init
-    preds = model(x)                        # model predictions
--- week1/train.py
+++ week1/train.py
@@ -1,178 +1,76 @@
 import torch
-from tqdm import tqdm
-import torch.nn as nn
-import torch.optim as optim
-from model import NetSquared
-from torchvision.datasets import ImageFolder
+from torch import nn
 from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from model import NeuralNetwork
+from torchvision.datasets import ImageFolder
 from torchvision import transforms
-import matplotlib.pyplot as plt
 
-# Hyperparameters etc.
-LEARNING_RATE = 1e-4
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 16
-NUM_EPOCHS = 10
-NUM_WORKERS = 2
-PIN_MEMORY = True       # To speed up the file transfer to CPU to GPU
-LOAD_MODEL = False      # If True the stored weights will be laoded
-TRAIN_IMG_DIR = "MIT_split/train/"
-TEST_IMG_DIR = "MIT_split/test/"
+from tqdm import tqdm
 
-def train_fn(loader, model, optimizer, loss_fn, scaler, device, epoch_num):
-    """
-    Function to train the model with one epoch
-    :param loader: Dataloder, train dataloader
-    :param model: object, model to train
-    :param optimizer: optimizer object
-    :param loss_fn: loss
-    :param scaler: scaler object
-    :param device: string ('cuda' or cpu')
-    :param epoch_num: int number of epoch in which the model is going to be trained
-    :return: float, float (accuracy, loss)
-    """
+TRAIN_DATASET = "./MIT_split/train"
+TEST_DATASET = "./MIT_split/test"
+BATCH_SIZE = 32
+NUM_EPOCHS = 200
 
-    model = model.to(device=device)     # Put model in DEVICE (recommended in GPU)
-    model.train()                       # Train mode
+tf = transforms.Compose([
+    transforms.ToTensor()
+])
 
-    loop = tqdm(loader, desc=f'EPOCH {epoch_num} TRAIN')  # Create the tqdm bar for visualizing the progress.
+train_dataset = ImageFolder(TRAIN_DATASET, transform=tf)    # Create the train dataset
+test_dataset = ImageFolder(TEST_DATASET, transform=tf)      # Create the test dataset
 
-    correct = 0     # accumulated correct predictions
-    total = 0       # accumulated total predictions
-    acc_loss = 0    # accumulated loss
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, pin_memory=True, shuffle=True)  # Create the train dataloader
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, pin_memory=True)                  # Create the test dataloader
 
-    # Loop to obtain the batches of images and labels
-    for (data, targets) in loop:
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
 
-        data = data.to(device=device)           # Batch of images to DEVICE, where the model is
-        targets = targets.to(device=device)     # Batch of labels to DEVICE, where the model is
+model = NeuralNetwork().to(device)
+print(model)
 
-        optimizer.zero_grad()               # Initialize the gradients
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adagrad(model.parameters(), lr=0.01)
 
-        output = model(data)                # Output of the model (logits).
-        loss = loss_fn(output, targets)     # Compute the loss between the output and the ground truth
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
 
-        _, predictions = torch.max(output.data, 1)  # Obtain the classes with higher probability (predicted classes)
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
 
-        total += data.size(0)                               # subtotal of the predictions
-        correct += (predictions == targets).sum().item()    # subtotal of the correct predictions
-        acc_loss += loss.item()                             # subtotal of the correct losses
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-        scaler.scale(loss).backward()   # compute the backward stage updating the weights of the model
-        scaler.step(optimizer)          # using the Gradient Scaler
-        scaler.update()
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-        loop.set_postfix(acc=correct/total, loss=acc_loss/total)  # set current accuracy and loss
-
-    return correct/total, acc_loss/total
-
-def eval_fn(loader, model, loss_fn, device, epoch_num):
-    """
-    Function to evaluate the model
-    :param loader: Dataloader, test dataloader
-    :param model: object, model to evaluate
-    :param loss_fn: loss
-    :param device: string ('cuda' or cpu')
-    :param epoch_num: int number of epoch in which the model is going to be evaluated
-    :return: float, float (accuracy, loss)
-    """
-
-    model = model.to(device=device)     # Put model in DEVICE (recommended in GPU)
-    model.eval()                        # Put the model in evaluation mode
-
-    loop = tqdm(loader, desc=f'EPOCH {epoch_num}  TEST')  # Create the tqdm bar for visualizing the progress.
-
-    correct = 0     # accumulated correct predictions
-    total = 0       # accumulated total predictions
-    acc_loss = 0    # accumulated loss
-
-    # Do not compute the gradients to go faster as we are not in training
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
     with torch.no_grad():
-        # Iterate through the batches
-        for (data, targets) in loop:
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-            data = data.to(device=device)           # Batch of images to DEVICE, where the model is
-            targets = targets.to(device=device)     # Batch of labels to DEVICE, where the model is
-
-            output = model(data)                    # Output of the model (logits).
-            loss = loss_fn(output, targets)         # Compute the loss between the output and the ground truth
-
-            _, predictions = torch.max(output.data, 1)  # Obtain the classes with higher probability
-
-            total += data.size(0)                               # subtotal of the predictions
-            correct += (predictions == targets).sum().item()    # subtotal of the correct predictions
-            acc_loss += loss.item()                             # subtotal of the correct losses
-
-            loop.set_postfix(acc=correct/total, loss=acc_loss/total)    # set current accuracy and loss
-
-    return correct/total, acc_loss/total
-
-def main():
-    # Find which device is used
-    if torch.cuda.is_available() and DEVICE=="cuda":
-        print(f'Training the model in {torch.cuda.get_device_name(torch.cuda.current_device())}')
-    else:
-        print('CAREFUL!! Training the model with CPU')
-
-    # Transform the output of the Dataset object into Tensor
-    tf = transforms.Compose([
-        transforms.ToTensor()
-    ])
-
-    model = NetSquared()    # Create the model
-
-    # Load the stored weight if LOAD_MODEL==True
-    if LOAD_MODEL:
-        checkpoint = torch.load("NetSquared_checkpoint.pth.tar")
-        model.load_state_dict(checkpoint["state_dict"])
-        print('Model loaded...')
-
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)    # Initialize the model
-    loss_fn = nn.CrossEntropyLoss()                                 # Initialize the loss
-    scaler = torch.cuda.amp.GradScaler()                            # Initialize the Scaler
-
-    # SCALER EXPLANATION: If the forward pass for a particular operation has float16 inputs, the backward pass will
-    # produce float16 gradients, which with small magnitudes may not be representable for float16. Gradient Scaling
-    # multiply the the network "losses" by a scale factor and invokes a backward pass on the scaled loss(es)
-    # https://pytorch.org/docs/stable/amp.html#torch.cuda.amp.GradScaler
-
-    train_dataset = ImageFolder(TRAIN_IMG_DIR, transform=tf)    # Create the train dataset
-    test_dataset = ImageFolder(TEST_IMG_DIR, transform=tf)      # Create the test dataset
-
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, pin_memory=True, shuffle=True)  # Create the train dataloader
-    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, pin_memory=True)                  # Create the test dataloader
-
-    train_metrics = {'accuracy': [], 'loss': []}
-    test_metrics = {'accuracy': [], 'loss': []}
-
-    # Iterate as many times as NUM_EPOCHS
-    for epoch in range(NUM_EPOCHS):
-
-        acc, loss = train_fn(train_loader, model, optimizer, loss_fn, scaler, DEVICE, epoch)    # Train the model
-        train_metrics['accuracy'].append(acc)   # Append train accuracy
-        train_metrics['loss'].append(loss)      # Append train accuracy
-
-        acc, loss = eval_fn(test_loader, model, loss_fn, DEVICE, epoch)                         # Validate the model in the test set
-        test_metrics['accuracy'].append(acc)    # Append test accuracy
-        test_metrics['loss'].append(loss)       # Append test accuracy
-
-        # If the validation accuracy is the best one so far, save the model
-        if (acc == max(test_metrics['accuracy'])) and (epoch > 0):
-            checkpoint = {
-                "state_dict": model.state_dict(),
-                "optimizer": optimizer.state_dict(),
-            }
-            torch.save(checkpoint, "NetSquared_checkpoint.pth.tar")
-            print('Model saved...')
-
-    # Plot accuracies and losses
-    plt.subplot(121)
-    plt.plot(train_metrics['accuracy'])
-    plt.plot(test_metrics['accuracy'])
-    plt.subplot(122)
-    plt.plot(train_metrics['loss'])
-    plt.plot(test_metrics['loss'])
-    plt.show()
-
-if __name__ == "__main__":
-    main()
+for t in tqdm(range(NUM_EPOCHS)):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_loader, model, loss_fn, optimizer)
+    test(test_loader, model, loss_fn)
+print("Done!")
